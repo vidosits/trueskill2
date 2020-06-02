2ac7a7f9e8b795db08ce564a7f0e191520b6d68a
András Vidosits <vidosits.andras@gmail.com>
2020-06-02 19:12:48 +0200

    put all stat variables into a variablearray

--- Classes.cs
+++ Classes.cs
@@ -6,28 +6,28 @@ namespace ts.core
     public class PlayerStat
     {
         public int? HeroId { get; set; }
-        public int? Kills { get; set; }
-        public int? Deaths { get; set; }
-        public int? Assists { get; set; }
-        public int? Level { get; set; }
+        public double? Kills { get; set; }
+        public double? Deaths { get; set; }
+        public double? Assists { get; set; }
+        public double? Level { get; set; }
         public double? Gpm { get; set; }
         public double? Xpm { get; set; }
-        public int? Networth { get; set; }
-        public int? CreepKills { get; set; }
-        public int? CreepDenies { get; set; }
-        public int? DamageDealt { get; set; }
-        public int? HealingDone { get; set; }
-        public int? ObserversKilled { get; set; }
-        public int? ObserversPlaced { get; set; }
-        public int? SentriesKilled { get; set; }
-        public int? SentriesPlaced { get; set; }
-        public int? CampsStacked { get; set; }
+        public double? Networth { get; set; }
+        public double? CreepKills { get; set; }
+        public double? CreepDenies { get; set; }
+        public double? DamageDealt { get; set; }
+        public double? HealingDone { get; set; }
+        public double? ObserversKilled { get; set; }
+        public double? ObserversPlaced { get; set; }
+        public double? SentriesKilled { get; set; }
+        public double? SentriesPlaced { get; set; }
+        public double? CampsStacked { get; set; }
     }
 
     public class Match
     {
         public int Id { get; set; }
-        public DateTime? Date {get; set;}
+        public DateTime Date {get; set;}
         public Dictionary<int, IList<int>> Rosters { get; set; }
         // public Dictionary<int, int> Scores { get; set; }
         public int SeriesId { get; set; }
--- Program.cs
+++ Program.cs
@@ -10,20 +10,8 @@ namespace ts.core
         private static void Main()
         {
             // Trueskill2.Test();
-            // Trueskill2.Run();
+            Trueskill2.Run();
             // OnlineLearning.Run();
-            
-            var rawMatches = ReadMatchesFromFile("/mnt/win/Andris/Work/WIN/trueskill/ts.core/abios_dota_matches_with_stats.json");
-            Console.WriteLine("OK.");
-        }
-
-        private static IEnumerable<Match> ReadMatchesFromFile(string fileName)
-        {
-            using (var r = new StreamReader(fileName))
-            {
-                Console.Write("Reading matches from file...");
-                return JsonConvert.DeserializeObject<List<Match>>(r.ReadToEnd());
-            }
         }
     }
 }
